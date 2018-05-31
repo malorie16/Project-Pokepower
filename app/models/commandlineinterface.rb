d431ec305e0dce7e4fbe8b672c387169fed02910
pablopawpaw <lpault29@gmail.com>
2018-05-31 17:23:11 -0400

    #view team stats

--- app/models/commandlineinterface.rb
+++ app/models/commandlineinterface.rb
@@ -70,38 +70,47 @@ class CommandLineInterface
      team.each do |poke|
        puts "#{counter += 1}. #{poke}"
      end
-    puts "Would you like to see any of these guys' stats? (Y or N)"
-    #method for stats
+     view_team_stats(user)
+  end
+
+  def view_team_stats(trainer)
+    #for specific user's team
+    puts "Would you like to these guys' stats? (Y or N)"
     user_input = gets.chomp
     if user_input.upcase == "Y"
-      view_team_stats(team)
-    else
-      show_menu(user)
+      puts "Get ready for your dream team's stats!"
+      trainer.pokemons.each do |pokemon|
+        stats = <<-STATS
+        _____________________________________________________________________
+        | Name: #{pokemon.name}
+        | Description: #{pokemon.description}
+        | Height: #{pokemon.height}
+        | Weight: #{pokemon.weight}
+        | Type 1: #{pokemon.type_1}
+        | Type 2: #{pokemon.type_2}
+        | Weakness 1: #{pokemon.weakness_1}
+        | Weakness 2: #{pokemon.weakness_2}
+        | Catch Rate: #{pokemon.catch_rate}
+        | Base HP: #{pokemon.base_hp}
+        | Base Attack: #{pokemon.base_attack}
+        | Base Defense: #{pokemon.base_defense}
+        | Base Sp Attack: #{pokemon.base_sp_attack}
+        | Base Sp Defense: #{pokemon.base_sp_defense}
+        | Base Speed: #{pokemon.base_speed}
+        | Base Exp: #{pokemon.base_experience}
+        |____________________________________________________________________
+        STATS
+        puts stats
+        show_menu(trainer)
+      end
+    elsif user_input.upcase == "N"
+      show_menu(trainer)
     end
   end
 
-  def view_team_stats(team)
-    #for specific user's team
-    #paul
-  end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   def view_stats #for submenu
     # need to create an exit option
-    #paul
+    
   end
 
 
